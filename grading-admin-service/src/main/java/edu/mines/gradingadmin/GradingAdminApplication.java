e825d72e61c64ca1c3dacf363bbf5abd97371ae1
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-01-02 15:59:02 -0700

    Add more setup things

--- grading-admin-service/src/main/java/edu/mines/gradingadmin/GradingAdminApplication.java
+++ grading-admin-service/src/main/java/edu/mines/gradingadmin/GradingAdminApplication.java
@@ -2,12 +2,20 @@ package edu.mines.gradingadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GradingAdminApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GradingAdminApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
