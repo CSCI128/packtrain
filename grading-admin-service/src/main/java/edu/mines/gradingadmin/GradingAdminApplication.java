f27872b912f6647f9d9b656da258d4231528e270
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-01-04 12:08:39 -0700

    Setup authentik with Spring resource server

--- grading-admin-service/src/main/java/edu/mines/gradingadmin/GradingAdminApplication.java
+++ grading-admin-service/src/main/java/edu/mines/gradingadmin/GradingAdminApplication.java
@@ -14,6 +14,11 @@ public class GradingAdminApplication {
 		SpringApplication.run(GradingAdminApplication.class, args);
 	}
 
+	@GetMapping(value = "/", produces = "text/plain")
+	public String home(String name) {
+		return "Hello!";
+	}
+
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
