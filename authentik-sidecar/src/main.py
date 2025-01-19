9cc89acc6e3c7f2dc47c108e13edbdb3834d3c78
Gregory Bell <trihard.studios@outlook.com>
2025-01-19 16:01:18 -0700

    Fix missing keys issue

--- authentik-sidecar/src/main.py
+++ authentik-sidecar/src/main.py
@@ -1,17 +1,33 @@
 import os
+import argparse
 
 from authentik import Authentik
 
 ENDPOINT = os.environ['AUTHENTIK_ENDPOINT']
 TOKEN = os.environ['AUTHENTIK_BOOTSTRAP_TOKEN']
 
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--recreate", action="store_true")
+
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    client = Authentik(ENDPOINT, TOKEN)
+    args = parse_args()
+
+    client = Authentik(ENDPOINT, TOKEN, recreate=args.recreate)
+
+    print(vars(client))
+
     if not client.wait_for_service():
         raise Exception("Failed to connect to service")
 
     provider_id = client.create_new_provider(
         "default-provider-authorization-explicit-consent",
+        "default-invalidation-flow",
         "grading_admin_provider",
         "grading_admin_provider"
     )
@@ -21,5 +37,3 @@ if __name__ == "__main__":
         "Grading Admin",
         "grading-admin"
     )
-
-
