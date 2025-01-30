d806d078399cb5dc8c483c34be9da19c027bf628
Gregory Bell <trihard.studios@outlook.com>
2025-01-29 22:09:45 -0700

    Add basic tests for security manager

--- grading-admin-service/src/main/java/edu/mines/gradingadmin/managers/SecurityManager.java
+++ grading-admin-service/src/main/java/edu/mines/gradingadmin/managers/SecurityManager.java
@@ -19,6 +19,7 @@ import java.util.Optional;
 public class SecurityManager {
     private final UserService userService;
     private Optional<JwtAuthenticationToken> principal;
+
     @Getter
     private User user;
 
@@ -26,6 +27,8 @@ public class SecurityManager {
     public SecurityManager(UserService userService) {
         this.userService = userService;
         principal = Optional.empty();
+
+        user = null;
     }
 
     public void setPrincipalFromRequest(HttpServletRequest request){
@@ -35,6 +38,20 @@ public class SecurityManager {
 
     }
 
+    /**
+     * This function reads the user information from the JWT on the request.
+     * At a high level, this attempts to look up the user by their oauth user id (the UUID that Authenik assigns them).
+     * <br></br>
+     * If that fails (the account may have been created via a canvas import or manually via the /admin/users route),
+     * then attempt to look up the user by their cwid.
+     * <br></br>
+     * If that fails, then this is a brand-new user, and we need to create their corresponding account in the db.
+     * <br></br>
+     * If the user is missing any claims in their JWT, then they will be denied.
+     *
+     *
+     * @throws AccessDeniedException if the user is not allowed to access it for whatever reason
+     */
     public void readUserFromRequest() throws AccessDeniedException {
         if (user != null){
             return;
@@ -47,7 +64,9 @@ public class SecurityManager {
         final Jwt token = principal.get().getToken();
 
         String oauthId = token.getSubject();
-        String cwid = token.getClaimAsString("cwid");
+        String cwid = Optional
+                .ofNullable(token.getClaimAsString("cwid"))
+                .orElseThrow(() -> new AccessDeniedException("Missing required claim: 'cwid'"));
 
         user = userService.getUserByOauthId(oauthId)
                 .or(() -> attemptToLinkExistingUser(cwid, oauthId))
@@ -55,9 +74,18 @@ public class SecurityManager {
                 .orElseThrow(() -> new AccessDeniedException("Failed to authorized user"));
     }
 
+    /**
+     * This function attempts to link the oauth id to an existing user based on the cwid.
+     * <br></br>
+     * If the user does not exist, then an empty optional is returned.
+     *
+     * @param cwid the user's cwid as claimed by the JWT
+     * @param oauthId the users oauth ID (the subject field)
+     * @return An optional populated with the current user, or an empty optional if the user
+     */
     private Optional<User> attemptToLinkExistingUser(String cwid, String oauthId){
         Optional<User> loadedUser = userService.getUserByCwid(cwid);
-        // this is yucky
+
         if (loadedUser.isPresent()){
             loadedUser = userService.linkCwidToOauthId(loadedUser.get(), oauthId);
         }
@@ -65,17 +93,35 @@ public class SecurityManager {
         return loadedUser;
     }
 
-    private Optional<User> attemptToCreateNewUser(final Jwt token, final String oauthId, final String cwid) {
-        String email = token.getClaimAsString("email");
-        String name = token.getClaimAsString("name");
-        boolean isAdmin = token.getClaim("is_admin");
 
+    /**
+     * This function attempts to create a new user in the database given a JWT and its claims.
+     * <br></br>
+     * The JWT must have the email claim, the is_admin claim, and the name claim to be created.
+     *
+     * @param token The JWT token
+     * @param oauthId the oauth id to link this newly created user to
+     * @param cwid the user's cwid
+     * @return a populated optional with the newly created user. It will only be empty
+     * @throws AccessDeniedException if we are missing any of the required claims
+     */
+    private Optional<User> attemptToCreateNewUser(final Jwt token, final String oauthId, final String cwid) throws AccessDeniedException {
+        String email = Optional
+                .ofNullable(token.getClaimAsString("email"))
+                .orElseThrow(() -> new AccessDeniedException("Missing required claim: 'email'"));
+
+        String name = Optional
+                .ofNullable(token.getClaimAsString("name"))
+                .orElseThrow(() -> new AccessDeniedException("Missing required claim: 'name'"));
+        boolean isAdmin = Optional
+                .ofNullable((Boolean) token.getClaim("is_admin"))
+                .orElseThrow(() -> new AccessDeniedException("Missing required claim: 'is_admin'"));
 
         return userService.createNewUser(cwid, isAdmin, name, email, oauthId);
     }
 
 
-    private String getCwid() throws AccessDeniedException{
+    public String getCwid() throws AccessDeniedException{
         if (user == null){
             throw new AccessDeniedException("No user context set.");
         }
@@ -83,7 +129,7 @@ public class SecurityManager {
         return user.getCwid();
     }
 
-    public boolean getIsUser(){
+    public boolean getIsUser() throws AccessDeniedException {
         if (user == null){
             throw new AccessDeniedException("No user context set.");
         }
@@ -91,7 +137,7 @@ public class SecurityManager {
         return user.isUser();
     }
 
-    public boolean getIsAdmin(){
+    public boolean getIsAdmin() throws AccessDeniedException {
         if (user == null){
             throw new AccessDeniedException("No user context set.");
         }
