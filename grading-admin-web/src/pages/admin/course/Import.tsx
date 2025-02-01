09985d7f176566041f1b49968b309ca31ce9ada2
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-02-01 11:37:52 -0700

    Implement basic course pages

---
+++ grading-admin-web/src/pages/admin/course/Import.tsx
@@ -0,0 +1,32 @@
+import { Container } from "@mantine/core";
+
+export function CoursePage() {
+  //   const { data, error, isLoading } = $api.useQuery(
+  //     "put",
+  //     "/admin/course/new/{canvas_id}",
+  //     {
+  //       params: {
+  //         path: { canvas_id: "5" },
+  //       },
+  //     }
+  //   );
+
+  //   if (isLoading || !data) return "Loading...";
+
+  //   if (error) return `An error occured: ${error}`;
+
+  //   return (
+  //     <>
+  //       <div>{data.name}</div>
+  //     </>
+  //   );
+
+  return (
+    <>
+      <Container size="xs">
+        xs Container
+        <p>Hello</p>
+      </Container>
+    </>
+  );
+}
