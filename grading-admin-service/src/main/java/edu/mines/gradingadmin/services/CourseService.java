ad7a7f1f81d3e4fdda0ac9eca94c1036a4d4756f
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-02-02 11:37:25 -0700

    Add courseservice

---
+++ grading-admin-service/src/main/java/edu/mines/gradingadmin/services/CourseService.java
@@ -0,0 +1,22 @@
+package edu.mines.gradingadmin.services;
+
+import edu.mines.gradingadmin.models.Course;
+import edu.mines.gradingadmin.repositories.CourseRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseService {
+
+    private final CourseRepo courseRepo;
+
+    public CourseService(CourseRepo courseRepo) {
+        this.courseRepo = courseRepo;
+    }
+
+    public Optional<List<Course>> getCourses() {
+        return Optional.of(courseRepo.getCourses());
+    }
+}
