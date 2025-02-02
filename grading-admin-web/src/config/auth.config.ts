1f1fb5c2d3581dbdf7277b14a2c10f02b667c08d
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-02-02 15:55:33 -0700

    Add more auth stuff to env

--- grading-admin-web/src/config/auth.config.ts
+++ grading-admin-web/src/config/auth.config.ts
@@ -2,10 +2,12 @@ export const AUTH_CONFIG = {
   authority:
     import.meta.env.VITE_OAUTH_URL ||
     "https://localhost.dev/auth/application/o/grading-admin/",
-  client_id: "grading_admin_provider",
+  client_id: import.meta.env.VITE_CLIENT_ID || "grading_admin_provider",
   redirect_uri: import.meta.env.VITE_REDIRECT_URI || "https://localhost.dev/",
   response_type: "code",
-  scope: "openid is_admin cwid email profile offline_access",
+  scope:
+    import.meta.env.VITE_AUTH_SCOPES ||
+    "openid is_admin cwid email profile offline_access",
   post_logout_redirect_uri:
     import.meta.env.VITE_LOGOUT_REDIRECT_URI || "https://localhost.dev/",
   code_challenge_method: "S256",
