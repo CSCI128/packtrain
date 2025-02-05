2069cefecf291ea311868752520a0284b3e59c29
Gregory Bell <trihard.studios@outlook.com>
2025-02-04 21:09:28 -0700

    Basic reads from canvas

---
+++ grading-admin-service/src/main/java/edu/mines/gradingadmin/config/EndpointConfig.java
@@ -0,0 +1,32 @@
+package edu.mines.gradingadmin.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.beans.InvalidPropertyException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+import java.util.InvalidPropertiesFormatException;
+
+@Configuration
+public class EndpointConfig {
+    @AllArgsConstructor
+    @Getter
+    public static class CanvasConfig{
+        private URI endpoint;
+    }
+
+    @Bean
+    public CanvasConfig configureCanvas(
+            @Value("${grading-admin.external-service.canvas.endpoint}") URI endpoint
+    ) throws InvalidPropertiesFormatException {
+        if (endpoint == null){
+            throw new InvalidPropertiesFormatException("Canvas endpoint not defined");
+        }
+        return new CanvasConfig(endpoint);
+    }
+
+}
--- grading-admin-service/src/main/java/edu/mines/gradingadmin/services/CanvasService.java
+++ grading-admin-service/src/main/java/edu/mines/gradingadmin/services/CanvasService.java
@@ -1,90 +1,118 @@
 package edu.mines.gradingadmin.services;
 
 
-//import edu.ksu.canvas.CanvasApiFactory;
-//import edu.ksu.canvas.interfaces.CourseReader;
-//import edu.ksu.canvas.oauth.NonRefreshableOauthToken;
-//import edu.ksu.canvas.oauth.OauthToken;
-//import edu.ksu.canvas.requestOptions.GetSingleCourseOptions;
-//import edu.ksu.canvas.requestOptions.ListCurrentUserCoursesOptions;
-//import edu.mines.gradingadmin.data.Course;
-//import edu.mines.gradingadmin.managers.SecurityManager;
-//import edu.mines.gradingadmin.models.ExternalSource;
-//import edu.mines.gradingadmin.models.CredentialType;
-//import edu.mines.gradingadmin.repositories.ExternalSourceRepo;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Service;
-//
-//import java.io.IOException;
-//import java.util.List;
-//import java.util.Optional;
-//import java.util.UUID;
-//
-//@Slf4j
-//@Service
-//public class CanvasService {
-//    private final boolean enabled;
-//    private final SecurityManager manager;
-//    private final CanvasApiFactory canvasApiFactory;
-//    private final String baseEndpoint;
-//
-//    public CanvasService(SecurityManager manager, ExternalSourceRepo externalSourceRepo) {
-//        this.manager = manager;
-//
-//        Optional<ExternalSource> source = externalSourceRepo.getByType(CredentialType.CANVAS);
-//
-//        enabled = source.isPresent();
-//
-//        baseEndpoint = enabled ? source.get().getEndpoint() : "disabled";
-//
-//        canvasApiFactory = new CanvasApiFactory(baseEndpoint);
-//    }
-//
-//    public List<Course> getAllAvailableCourses(){
-//        OauthToken canvasToken = new NonRefreshableOauthToken(manager.getCredential(baseEndpoint, UUID.randomUUID()));
-//
-//        CourseReader reader = canvasApiFactory.getReader(CourseReader.class, canvasToken);
-//
-//        ListCurrentUserCoursesOptions params = new ListCurrentUserCoursesOptions()
-//                .withEnrollmentType(ListCurrentUserCoursesOptions.EnrollmentType.TEACHER);
-//
-//        List<edu.ksu.canvas.model.Course> courses = List.of();
-//        try {
-//            courses = reader.listCurrentUserCourses(params);
-//        } catch (IOException e) {
-//            log.error("Failed to get courses from Canvas");
-//        }
-//
-//        return courses.stream()
-//                .map(
-//                course -> new Course()
-//                        .code(course.getCourseCode())
-//                        .name(course.getName())
-//                        .canvasId(course.getId().toString())
-//                )
-//                .toList();
-//    }
-//
-//    public Course getCourse(String id){
-//        OauthToken canvasToken = new NonRefreshableOauthToken(manager.getCredential(baseEndpoint, UUID.randomUUID()));
-//
-//        CourseReader reader = canvasApiFactory.getReader(CourseReader.class, canvasToken);
-//
-//        Optional<edu.ksu.canvas.model.Course> course = Optional.empty();
-//
-//        GetSingleCourseOptions params = new GetSingleCourseOptions(id)
-//                .includes(List.of(GetSingleCourseOptions.Include.SECTIONS));
-//
-//        try {
-//            course = reader.getSingleCourse(params);
-//        } catch (IOException e) {
-//            log.error("Failed to get course from Canvas");
-//        }
-//
-//    }
-//
-//
-//
-//
-//
-//}
+import edu.ksu.canvas.CanvasApiFactory;
+import edu.ksu.canvas.interfaces.CourseReader;
+import edu.ksu.canvas.interfaces.SectionReader;
+import edu.ksu.canvas.interfaces.UserReader;
+import edu.ksu.canvas.model.Course;
+import edu.ksu.canvas.model.Section;
+import edu.ksu.canvas.model.User;
+import edu.ksu.canvas.oauth.NonRefreshableOauthToken;
+import edu.ksu.canvas.oauth.OauthToken;
+import edu.ksu.canvas.requestOptions.GetSingleCourseOptions;
+import edu.ksu.canvas.requestOptions.GetUsersInCourseOptions;
+import edu.ksu.canvas.requestOptions.ListCurrentUserCoursesOptions;
+import edu.mines.gradingadmin.config.EndpointConfig;
+import edu.mines.gradingadmin.managers.SecurityManager;
+import edu.mines.gradingadmin.models.CredentialType;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class CanvasService {
+    private final SecurityManager manager;
+    private final EndpointConfig.CanvasConfig config;
+    private final CanvasApiFactory canvasApiFactory;
+
+    public CanvasService(SecurityManager manager, EndpointConfig.CanvasConfig config) {
+        this.manager = manager;
+        this.config = config;
+
+        canvasApiFactory = new CanvasApiFactory(this.config.getEndpoint().toString());
+    }
+
+    public List<Course> getAllAvailableCourses(){
+        OauthToken canvasToken = new NonRefreshableOauthToken(manager.getCredential(CredentialType.CANVAS, UUID.randomUUID()));
+
+        CourseReader reader = canvasApiFactory.getReader(CourseReader.class, canvasToken);
+
+        ListCurrentUserCoursesOptions params = new ListCurrentUserCoursesOptions()
+                .withEnrollmentType(ListCurrentUserCoursesOptions.EnrollmentType.TEACHER);
+
+        List<Course> courses = List.of();
+        try {
+            courses = reader.listCurrentUserCourses(params);
+        } catch (IOException e) {
+            log.error("Failed to get courses from Canvas");
+        }
+
+        return courses;
+    }
+
+    public Optional<Course> getCourse(String id){
+        OauthToken canvasToken = new NonRefreshableOauthToken(manager.getCredential(CredentialType.CANVAS, UUID.randomUUID()));
+
+        CourseReader reader = canvasApiFactory.getReader(CourseReader.class, canvasToken);
+
+        Optional<Course> course = Optional.empty();
+
+        GetSingleCourseOptions params = new GetSingleCourseOptions(id);
+
+        try {
+            course = reader.getSingleCourse(params);
+        } catch (IOException e) {
+            log.error("Failed to get course from Canvas");
+        }
+
+        return course;
+    }
+
+    public List<User> getCourseMembers(String id){
+        OauthToken canvasToken = new NonRefreshableOauthToken(manager.getCredential(CredentialType.CANVAS, UUID.randomUUID()));
+
+        UserReader reader = canvasApiFactory.getReader(UserReader.class, canvasToken);
+
+        List<User> users = List.of();
+
+        GetUsersInCourseOptions params = new GetUsersInCourseOptions(id)
+                .enrollmentState(List.of(GetUsersInCourseOptions.EnrollmentState.ACTIVE))
+                .enrollmentType(List.of(GetUsersInCourseOptions.EnrollmentType.TEACHER, GetUsersInCourseOptions.EnrollmentType.STUDENT))
+                .include(List.of(GetUsersInCourseOptions.Include.ENROLLMENTS));
+
+        try {
+            // omg i love you KSU.
+            // automatic handling of result pages <3
+            users = reader.getUsersInCourse(params);
+        } catch (IOException e){
+            log.error("Failed to get users from in course from Canvas");
+        }
+
+        return users;
+    }
+
+    public List<Section> getCourseSections(String id){
+        OauthToken canvasToken = new NonRefreshableOauthToken(manager.getCredential(CredentialType.CANVAS, UUID.randomUUID()));
+
+        SectionReader reader = canvasApiFactory.getReader(SectionReader.class, canvasToken);
+
+        List<Section> sections = List.of();
+
+        try {
+            // omg i love you KSU.
+            // automatic handling of result pages <3
+            sections = reader.listCourseSections(id, List.of());
+        } catch (IOException e){
+            log.error("Failed to get sections for course from Canvas");
+        }
+
+        return sections;
+    }
+
+}
