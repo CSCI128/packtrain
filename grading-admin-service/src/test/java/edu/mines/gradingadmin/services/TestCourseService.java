7f8fdeaa8b4f44db0cd8864963e7dd18d008c310
Gregory Bell <trihard.studios@outlook.com>
2025-02-05 17:10:52 -0700

    write security manager tests

--- grading-admin-service/src/test/java/edu/mines/gradingadmin/services/TestCourseService.java
+++ grading-admin-service/src/test/java/edu/mines/gradingadmin/services/TestCourseService.java
@@ -3,7 +3,9 @@ package edu.mines.gradingadmin.services;
 import edu.mines.gradingadmin.containers.PostgresTestContainer;
 import edu.mines.gradingadmin.managers.SecurityManager;
 import edu.mines.gradingadmin.models.Course;
+import edu.mines.gradingadmin.repositories.CourseMemberRepo;
 import edu.mines.gradingadmin.repositories.CourseRepo;
+import edu.mines.gradingadmin.repositories.SectionRepo;
 import edu.mines.gradingadmin.seeders.CanvasSeeder;
 import edu.mines.gradingadmin.seeders.CourseSeeders;
 import jakarta.transaction.Transactional;
@@ -14,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 @Transactional
@@ -21,9 +24,10 @@ public class TestCourseService implements PostgresTestContainer, CanvasSeeder {
 
     @Autowired
     private CourseSeeders courseSeeders;
+
     @Autowired
     private CourseRepo courseRepo;
-    @Autowired
+
     private CourseService courseService;
 
     @Mock
@@ -32,6 +36,12 @@ public class TestCourseService implements PostgresTestContainer, CanvasSeeder {
     @Mock
     private SecurityManager securityManager;
 
+    @Autowired
+    private SectionRepo sectionRepo;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private CourseMemberRepo courseMemberRepo;
 
 
     @BeforeAll
@@ -43,6 +53,9 @@ public class TestCourseService implements PostgresTestContainer, CanvasSeeder {
     @BeforeEach
     void setup(){
         applyMocks(canvasService);
+
+        courseService = new CourseService(securityManager, courseRepo, canvasService, new SectionService(sectionRepo, canvasService), userService, courseMemberRepo);
+
     }
 
     @AfterEach
@@ -73,7 +86,11 @@ public class TestCourseService implements PostgresTestContainer, CanvasSeeder {
     }
 
     @Test
-    void verifyAddMembersToCourse(){
+    void verifyImportCourseFromCanvas(){
+        // I am aware that this is hot garbage,
+        // I need to take a step back from this and do another refactoring pass on it.
+        Optional<Course> importedCourse = courseService.importCourseFromCanvas(String.valueOf(course1Id));
 
+        Assertions.assertTrue(importedCourse.isPresent());
     }
 }
