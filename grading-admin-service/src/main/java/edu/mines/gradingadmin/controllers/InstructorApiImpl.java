e4172daabbf1578662be9693d644230fff549dca
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-02-15 15:22:48 -0700

    Course information routes (#83)
    * add routes
    
    * fix routes
    
    * add routes

--- grading-admin-service/src/main/java/edu/mines/gradingadmin/controllers/InstructorApiImpl.java
+++ grading-admin-service/src/main/java/edu/mines/gradingadmin/controllers/InstructorApiImpl.java
@@ -1,10 +1,15 @@
 package edu.mines.gradingadmin.controllers;
 
 import edu.mines.gradingadmin.api.InstructorApiDelegate;
+import edu.mines.gradingadmin.data.AssignmentDTO;
 import edu.mines.gradingadmin.data.CourseDTO;
+import edu.mines.gradingadmin.data.CourseMemberDTO;
 import edu.mines.gradingadmin.data.PolicyDTO;
 import edu.mines.gradingadmin.managers.SecurityManager;
+import edu.mines.gradingadmin.models.Course;
+import edu.mines.gradingadmin.models.CourseMember;
 import edu.mines.gradingadmin.models.Policy;
+import edu.mines.gradingadmin.models.Section;
 import edu.mines.gradingadmin.services.CourseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +18,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
+
+import static java.util.stream.Collectors.toList;
 
 @Controller
 public class InstructorApiImpl implements InstructorApiDelegate {
@@ -23,6 +31,47 @@ public class InstructorApiImpl implements InstructorApiDelegate {
     public InstructorApiImpl(CourseService courseService, SecurityManager securityManager) {
         this.courseService = courseService;
         this.securityManager = securityManager;
+    }
+
+    @Override
+    public ResponseEntity<CourseDTO> getCourseInformationInstructor(String courseId) {
+        Optional<Course> course = courseService.getCourse(UUID.fromString(courseId));
+
+        if(course.isEmpty()) {
+            // need to do this with error controller
+            return ResponseEntity.badRequest().build();
+        }
+
+        Optional<CourseMember> courseMember = securityManager.getUser().getCourseMemberships().stream().filter(c -> c.getId().toString().equals(courseId)).findFirst();
+        if(courseMember.isEmpty()) {
+            // need to do this with error controller
+            return ResponseEntity.badRequest().build();
+        }
+
+        CourseDTO courseDTO = new CourseDTO()
+                .id(course.get().getId().toString())
+                .code(course.get().getCode())
+                .name(course.get().getName())
+                .term(course.get().getTerm())
+                .enabled(course.get().isEnabled())
+                .canvasId(course.get().getCanvasId())
+                .assignments(course.get().getAssignments().stream().map(assignment ->
+                    new AssignmentDTO()
+                            .category(assignment.getCategory())
+                            .dueDate(assignment.getDueDate())
+                            .unlockDate(assignment.getUnlockDate())
+                            .enabled(assignment.isEnabled())
+                            .points(assignment.getPoints())).toList())
+                .members(courseMember.get().getSections().stream().map(Section::getMembers).flatMap(Set::stream).map(member ->
+                    new CourseMemberDTO()
+                        .canvasId(member.getCanvasId())
+                        .courseRole(CourseMemberDTO.CourseRoleEnum.fromValue(member.getRole().getRole()))
+                        .cwid(member.getUser().getCwid())
+                        .sections(member.getSections().stream().map(Section::getName).toList()))
+                    .collect(toList()))
+                .sections(courseMember.get().getSections().stream().map(Section::getName).toList());
+
+        return ResponseEntity.ok(courseDTO);
     }
 
     @Override
