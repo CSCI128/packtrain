05b46e13fbd48559b138a85150a8b87b9d0b33ac
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-02-16 21:55:57 -0700

    Add assignment route (#90)
    * Implement add assignment things
    
    * Apply code review feedback
    
    * Add update assignment route
    
    * update to not double set the course side of the assignments
    
    * make tests test the correct thing
    
    * remove irrevent code
    
    ---------
    
    Co-authored-by: Gregory Bell <trihard.studios@outlook.com>

--- grading-admin-service/src/test/java/edu/mines/gradingadmin/services/TestAssignmentService.java
+++ grading-admin-service/src/test/java/edu/mines/gradingadmin/services/TestAssignmentService.java
@@ -1,27 +1,32 @@
 package edu.mines.gradingadmin.services;
 
 import edu.mines.gradingadmin.containers.PostgresTestContainer;
+import edu.mines.gradingadmin.data.AssignmentDTO;
 import edu.mines.gradingadmin.models.Assignment;
 import edu.mines.gradingadmin.models.Course;
 import edu.mines.gradingadmin.repositories.UserRepo;
 import edu.mines.gradingadmin.seeders.CourseSeeders;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootTest
+@Transactional
 public class TestAssignmentService implements PostgresTestContainer {
-    @Autowired
-    private CourseSeeders courseSeeders;
 
     @Autowired
     private AssignmentService assignmentService;
-
+    @Autowired
+    private CourseService courseService;
+    @Autowired
+    private CourseSeeders courseSeeders;
     @Autowired
     private UserRepo userRepo;
 
@@ -30,10 +35,32 @@ public class TestAssignmentService implements PostgresTestContainer {
         postgres.start();
     }
 
+    @BeforeEach
+    void setup() {
+
+    }
+
     @AfterEach
     void tearDown(){
         courseSeeders.clearAll();
         userRepo.deleteAll();
+    }
+
+    @Test
+    void verifyAddAssignments() {
+        Course course = courseSeeders.course1();
+
+        Optional<Assignment> assignment = assignmentService.addAssignmentToCourse(
+                course.getId().toString(),
+                "Studio 1",
+                2.0,
+                "Studios",
+                Instant.now(),
+                Instant.now()
+        );
+
+        Assertions.assertTrue(assignment.isPresent());
+        Assertions.assertTrue(assignment.get().isEnabled());
     }
 
     @Test
@@ -45,4 +72,31 @@ public class TestAssignmentService implements PostgresTestContainer {
         Assertions.assertEquals(course.getAssignments().size(), assignments.size());
     }
 
+    @Test
+    void verifyUpdateAssignment() {
+        Course course = courseSeeders.course1();
+
+        Optional<Assignment> assignment = assignmentService.addAssignmentToCourse(
+                course.getId().toString(),
+                "Studio 1",
+                2.0,
+                "Studios",
+                Instant.now(),
+                Instant.now()
+        );
+
+        Assertions.assertTrue(assignment.isPresent());
+
+        assignment = assignmentService.updateAssignment(course.getId().toString(),
+                assignment.get().getId().toString(),
+                assignment.get().getName(),
+                10,
+                assignment.get().getCategory(),
+                assignment.get().isEnabled(),
+                assignment.get().getDueDate(),
+                assignment.get().getUnlockDate());
+
+        Assertions.assertTrue(assignment.isPresent());
+        Assertions.assertEquals(10, assignment.get().getPoints());
+    }
 }
