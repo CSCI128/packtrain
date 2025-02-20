1375e32fbf1d136baa7ea0c352aae970f350cdfd
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-02-19 18:55:13 -0700

    Finish members page

--- grading-admin-web/src/pages/admin/Members.tsx
+++ grading-admin-web/src/pages/admin/Members.tsx
@@ -1,4 +1,4 @@
-import { Container, Divider, Table, Tabs, Text } from "@mantine/core";
+import { Container, ScrollArea, Table, Tabs, Text } from "@mantine/core";
 import { $api, store$ } from "../../api";
 
 export function MembersPage() {
@@ -8,9 +8,9 @@ export function MembersPage() {
     {
       params: {
         path: { course_id: store$.id.get() as string },
-      },
-      query: {
-        enrollments: ["tas", "students"],
+        query: {
+          enrollments: ["tas", "students"],
+        },
       },
     }
   );
@@ -22,9 +22,9 @@ export function MembersPage() {
   } = $api.useQuery("get", "/admin/courses/{course_id}/members", {
     params: {
       path: { course_id: store$.id.get() as string },
-    },
-    query: {
-      enrollments: ["instructors"],
+      query: {
+        enrollments: ["instructors"],
+      },
     },
   });
 
@@ -32,10 +32,6 @@ export function MembersPage() {
     return "Loading...";
 
   if (error || instructorError) return `An error occured: ${error}`;
-
-  console.log(data);
-
-  console.log(instructorData);
 
   const memberRows = data.map((element) => (
     <Table.Tr key={element.cwid}>
@@ -45,7 +41,7 @@ export function MembersPage() {
     </Table.Tr>
   ));
 
-  const instructorRows = data.map((element) => (
+  const instructorRows = instructorData.map((element) => (
     <Table.Tr key={element.cwid}>
       <Table.Td>{element.cwid}</Table.Td>
       <Table.Td>{element.course_role}</Table.Td>
@@ -60,8 +56,6 @@ export function MembersPage() {
           Course Members
         </Text>
 
-        <Divider my="sm" />
-
         <Tabs defaultValue="first" mt={10}>
           <Tabs.List>
             <Tabs.Tab value="first">Members</Tabs.Tab>
@@ -69,29 +63,33 @@ export function MembersPage() {
           </Tabs.List>
 
           <Tabs.Panel value="first" pt="xs">
-            <Table>
-              <Table.Thead>
-                <Table.Tr>
-                  <Table.Th>CWID</Table.Th>
-                  <Table.Th>Role</Table.Th>
-                  <Table.Th>Sections</Table.Th>
-                </Table.Tr>
-              </Table.Thead>
-              <Table.Tbody>{memberRows}</Table.Tbody>
-            </Table>
+            <ScrollArea h={500}>
+              <Table>
+                <Table.Thead>
+                  <Table.Tr>
+                    <Table.Th>CWID</Table.Th>
+                    <Table.Th>Role</Table.Th>
+                    <Table.Th>Sections</Table.Th>
+                  </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody>{memberRows}</Table.Tbody>
+              </Table>
+            </ScrollArea>
           </Tabs.Panel>
 
           <Tabs.Panel value="second" pt="xs">
-            <Table>
-              <Table.Thead>
-                <Table.Tr>
-                  <Table.Th>CWID</Table.Th>
-                  <Table.Th>Role</Table.Th>
-                  <Table.Th>Sections</Table.Th>
-                </Table.Tr>
-              </Table.Thead>
-              <Table.Tbody>{instructorRows}</Table.Tbody>
-            </Table>
+            <ScrollArea h={500}>
+              <Table>
+                <Table.Thead>
+                  <Table.Tr>
+                    <Table.Th>CWID</Table.Th>
+                    <Table.Th>Role</Table.Th>
+                    <Table.Th>Sections</Table.Th>
+                  </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody>{instructorRows}</Table.Tbody>
+              </Table>
+            </ScrollArea>
           </Tabs.Panel>
         </Tabs>
       </Container>
