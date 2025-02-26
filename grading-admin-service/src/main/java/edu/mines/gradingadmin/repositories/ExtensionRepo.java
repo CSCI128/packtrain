577a7b6c60e4bd0a10f09053ddce75dc0a93f96e
Kylee Wibbeler <kwibbeler@mines.edu>
2025-02-26 16:53:22 -0700

    Stub out the extension service, repo and test file so that I can use it in my migrations

---
+++ grading-admin-service/src/main/java/edu/mines/gradingadmin/repositories/ExtensionRepo.java
@@ -0,0 +1,13 @@
+package edu.mines.gradingadmin.repositories;
+
+import edu.mines.gradingadmin.models.Extension;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+import java.util.List;
+
+public interface ExtensionRepo extends CrudRepository<Extension, UUID> {
+    @Query("select a from extension a where a.migration.id=?1")
+    List<Extension> getExtensionByMigrationId(UUID migrationId);
+}
