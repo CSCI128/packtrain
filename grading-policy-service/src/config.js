7f3faf7e174bcb9f0360ed36ee53d9af7ce4a7a4
Gregory Bell <trihard.studios@outlook.com>
2025-02-27 09:31:31 -0700

    actually fully remove the JS frfr no cap no cap

--- grading-policy-service/src/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-const node_fs_1 = require("node:fs");
-const yaml_1 = require("yaml");
-exports.config = (0, yaml_1.parse)((0, node_fs_1.readFileSync)(process.argv[2], "utf-8"));
--- grading-policy-service/src/data/GradingStartDTO.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GlobalAssignmentMetadata = void 0;
-class GlobalAssignmentMetadata {
-}
-exports.GlobalAssignmentMetadata = GlobalAssignmentMetadata;
-class GradingStartDTO {
-}
-exports.default = GradingStartDTO;
