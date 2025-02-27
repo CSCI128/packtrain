554cefa673bc40c265cdbe59422e01eba1075d58
Kylee Wibbeler <kwibbeler@mines.edu>
2025-02-27 16:32:05 -0700

    Continue working on migration service

--- grading-admin-service/src/test/java/edu/mines/gradingadmin/services/TestExtensionService.java
+++ grading-admin-service/src/test/java/edu/mines/gradingadmin/services/TestExtensionService.java
@@ -1,10 +1,9 @@
 package edu.mines.gradingadmin.services;
 
 import edu.mines.gradingadmin.containers.PostgresTestContainer;
-import edu.mines.gradingadmin.models.Extension;
-import edu.mines.gradingadmin.models.Migration;
-import edu.mines.gradingadmin.models.User;
+import edu.mines.gradingadmin.models.*;
 import edu.mines.gradingadmin.repositories.ExtensionRepo;
+import edu.mines.gradingadmin.seeders.CourseSeeders;
 import edu.mines.gradingadmin.seeders.UserSeeders;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -13,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Set;
 
 @SpringBootTest
 public class TestExtensionService implements PostgresTestContainer {
@@ -21,6 +21,8 @@ public class TestExtensionService implements PostgresTestContainer {
 
     @Autowired
     private ExtensionRepo extensionRepo;
+    @Autowired
+    private CourseSeeders courseSeeders;
 
     @BeforeAll
     static void setupClass(){
@@ -33,7 +35,13 @@ public class TestExtensionService implements PostgresTestContainer {
     }
 
     // TO-DO: test that getAllExtensions does what is expected
-    // Will need to create a migration seed in order to write this test.
+    // Will need to create a migration seed in order to write this test I think
     // This test should be implemented when the ExtensionService is implemented
+    @Test
+    void verifyGetAllExtensions(){
+        Course course = courseSeeders.populatedCourse();
+        Set<Assignment> assignment = course.getAssignments();
+
+    }
 
 }
--- grading-admin-service/src/test/java/edu/mines/gradingadmin/services/TestMigrationService.java
+++ grading-admin-service/src/test/java/edu/mines/gradingadmin/services/TestMigrationService.java
@@ -1,6 +1,48 @@
 package edu.mines.gradingadmin.services;
 
 
-public class TestMigrationService {
+import edu.mines.gradingadmin.models.Course;
+import edu.mines.gradingadmin.models.Migration;
+import edu.mines.gradingadmin.repositories.ExtensionRepo;
+import edu.mines.gradingadmin.repositories.MigrationRepo;
+import edu.mines.gradingadmin.seeders.CourseSeeders;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static edu.mines.gradingadmin.containers.PostgresTestContainer.postgres;
+
+@SpringBootTest
+public class TestMigrationService {
+    @Autowired
+    MigrationService migrationService;
+
+    @Autowired
+    private MigrationRepo migrationRepo;
+    @Autowired
+    private CourseSeeders courseSeeders;
+
+    @BeforeAll
+    static void setupClass(){
+        postgres.start();
+    }
+
+    @AfterEach
+    void tearDown(){
+        migrationRepo.deleteAll();
+    }
+
+    @Test
+    void verifyGetAllMigration(){
+       Course course = courseSeeders.populatedCourse();
+       List<Migration> migrations = migrationService.getAllMigrations(course.getId().toString());
+       // need to think about how to assert this in a way that makes the most sense
+       Assertions.assertEquals();
+
+    }
 }
