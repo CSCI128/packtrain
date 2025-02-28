e295e11b952425f0a55619fa71b85c953778118a
Gregory Bell <trihard.studios@outlook.com>
2025-02-28 11:24:38 -0700

    remainder of CR changes

--- grading-policy-service/src/main.ts
+++ grading-policy-service/src/main.ts
@@ -5,24 +5,20 @@ import { readFileSync } from "node:fs";
 import { setup } from "./api/api";
 import { connect } from "./services/rabbitMqService";
 
-function main() {
-    const serverOpts: https.ServerOptions = {
-        key: readFileSync(config.securityConfig.serverKey),
-        cert: readFileSync(config.securityConfig.serverCert),
-        requestCert: true,
-        rejectUnauthorized: false,
-        ca: config.securityConfig.trustedCAs.map((ca) => readFileSync(ca)),
-    };
+const serverOpts: https.ServerOptions = {
+    key: readFileSync(config.securityConfig.serverKey),
+    cert: readFileSync(config.securityConfig.serverCert),
+    requestCert: true,
+    rejectUnauthorized: false,
+    ca: config.securityConfig.trustedCAs.map((ca) => readFileSync(ca)),
+};
 
-    const app = setup(config, express());
+const app = setup(config, express());
 
-    connect(config.rabbitMqConfig).then(() => {
-        console.log("Connection Established!");
-    });
+connect(config.rabbitMqConfig).then(() => {
+    console.log("Connection Established!");
+});
 
-    https.createServer(serverOpts, app).listen(config.port, () => {
-        console.log(`Listening on :${config.port}`);
-    });
-}
-
-main();
+https.createServer(serverOpts, app).listen(config.port, () => {
+    console.log(`Listening on :${config.port}`);
+});
