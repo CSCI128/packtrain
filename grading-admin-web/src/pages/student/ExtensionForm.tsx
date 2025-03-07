0faca1db44a6b7447575a6be1a3a0b2d282d46db
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-03-06 21:37:40 -0700

    Add other form and form toggle

--- grading-admin-web/src/pages/student/ExtensionForm.tsx
+++ grading-admin-web/src/pages/student/ExtensionForm.tsx
@@ -21,6 +21,7 @@ export function ExtensionForm() {
 
   const [originalDate, setOriginalDate] = useState<Date>(new Date());
   const [extensionDays, setExtensionDays] = useState<number>(1);
+  const [latePassView, setLatePassView] = useState<boolean>(true);
 
   const extensionForm = useForm({
     mode: "uncontrolled",
@@ -79,77 +80,106 @@ export function ExtensionForm() {
         <strong>Date</strong>: {new Date().toLocaleString()}
       </Text>
 
-      {/* <form onSubmit={latePassForm.onSubmit(submitLatePass)}></form> */}
-
       <InputWrapper label="Extension Type:">
         <SegmentedControl
-          onChange={() => console.log("hello")}
+          ml={5}
+          onChange={() => setLatePassView(!latePassView)}
           data={["Late Pass", "Extension"]}
         />
       </InputWrapper>
 
-      <form onSubmit={extensionForm.onSubmit(submitExtension)}>
-        <Stack>
-          <Select label="Assignment" placeholder="Pick value" data={["Test"]} />
+      {latePassView ? (
+        <form onSubmit={latePassForm.onSubmit(submitLatePass)}>
+          <Stack>
+            <Text>
+              You have <strong>X</strong> late passes remaining. Late passes are
+              five, free passes to use over the course of the semester to extend
+              your work.
+            </Text>
 
-          <Text>
-            Extensions are <strong>only</strong> for medical, excused or
-            extenuating personal circumstances.
-          </Text>
-          <Text>
-            This request will be sent to and reviewed by{" "}
-            <strong>Professor TODO</strong>.
-          </Text>
+            <Group>
+              <NumberInput
+                label="Days to extend:"
+                defaultValue={1}
+                onChange={() => setExtensionDays}
+                max={5}
+                min={1}
+              />
 
-          <Group>
-            {/* TODO more validation on this: maximums */}
-            <NumberInput
-              label="Days Requested"
-              defaultValue={1}
-              onChange={() => setExtensionDays}
-              max={5}
-              min={1}
-            />
-
+              <Text>
+                (<strong>X remaining</strong> after)
+              </Text>
+            </Group>
+          </Stack>
+        </form>
+      ) : (
+        <form onSubmit={extensionForm.onSubmit(submitExtension)}>
+          <Stack>
             <Select
-              label="Extension Reason"
+              label="Assignment"
               placeholder="Pick value"
-              data={[
-                "Tech Issues",
-                "Health-Related",
-                "Family Emergency",
-                "Personal",
-              ]}
+              data={["Test"]}
             />
+
+            <Text>
+              Extensions are <strong>only</strong> for medical, excused or
+              extenuating personal circumstances.
+            </Text>
+            <Text>
+              This request will be sent to and reviewed by{" "}
+              <strong>Professor TODO</strong>.
+            </Text>
+
+            <Group>
+              <NumberInput
+                label="Days Requested"
+                defaultValue={1}
+                onChange={() => setExtensionDays}
+                max={5}
+                min={1}
+              />
+
+              <Select
+                label="Extension Reason"
+                placeholder="Pick value"
+                data={[
+                  "Tech Issues",
+                  "Health-Related",
+                  "Family Emergency",
+                  "Personal",
+                ]}
+              />
+            </Group>
+
+            <Textarea label="Comments/Explanation" placeholder="Comments" />
+
+            <Text c="gray">
+              Original Due Date:{" "}
+              <strong>{originalDate.toLocaleString()}</strong>.
+            </Text>
+
+            <Text>
+              <strong>If approved</strong>, the assignment will be due{" "}
+              <strong>
+                {/* TODO update this when extensionDays updates */}
+                {new Date(
+                  originalDate.getTime() + extensionDays * 86400000
+                ).toLocaleString()}
+              </strong>
+              .
+            </Text>
+          </Stack>
+
+          <Group justify="flex-end" mt="md">
+            <Button component={Link} to="/" type="submit" color="gray">
+              Cancel
+            </Button>
+            <Button component={Link} to="/admin/home" type="submit">
+              Submit
+            </Button>
           </Group>
-
-          <Textarea label="Comments/Explanation" placeholder="Comments" />
-
-          <Text c="gray">
-            Original Due Date: <strong>{originalDate.toLocaleString()}</strong>.
-          </Text>
-
-          <Text>
-            <strong>If approved</strong>, the assignment will be due{" "}
-            <strong>
-              {/* TODO update this when extensionDays updates */}
-              {new Date(
-                originalDate.getTime() + extensionDays * 86400000
-              ).toLocaleString()}
-            </strong>
-            .
-          </Text>
-        </Stack>
-
-        <Group justify="flex-end" mt="md">
-          <Button component={Link} to="/" type="submit" color="gray">
-            Cancel
-          </Button>
-          <Button component={Link} to="/admin/home" type="submit">
-            Submit
-          </Button>
-        </Group>
-      </form>
+        </form>
+      )}
     </Container>
   );
 }
