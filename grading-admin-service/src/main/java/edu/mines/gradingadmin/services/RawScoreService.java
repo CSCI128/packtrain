51a375a49fc8588fa24f60a4d01baf3d4edb99b6
Swiffels <54492510+Swiffels@users.noreply.github.com>
2025-03-14 15:14:41 -0600

    Feat upload grade sheet (#156)
    * Add model and model enum
    
    * Feat raw score service read csv (#140)
    
    * Start on raw score service
    
    * Fix openapi snapshot issue and small edits on the upload function
    
    * Add test service java and fix bugs
    
    * Fix casing in query
    
    * Start working on tests and added more work on the service
    
    * expand on tests
    
    * Fix datatypes for times and Add null checks
    
    * Start work on applying feedback
    
    Still WIP, don't request review yet
    
    * Update to be inline with feedback
    
    * Fix small issues
    
    ---------
    
    Co-authored-by: kaedeng <kdeng@mines.edu>
    
    * Update raw score to work with submission status
    
    * Fix small issues
    
    ---------
    
    Co-authored-by: kaedeng <kdeng@mines.edu>

---
+++ grading-admin-service/src/main/java/edu/mines/gradingadmin/services/RawScoreService.java
@@ -0,0 +1,151 @@
+package edu.mines.gradingadmin.services;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvValidationException;
+import edu.ksu.canvas.model.Enrollment;
+import edu.mines.gradingadmin.models.CourseRole;
+import edu.mines.gradingadmin.models.RawScore;
+import edu.mines.gradingadmin.models.SubmissionStatus;
+import edu.mines.gradingadmin.repositories.RawScoreRepo;
+import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.index.qual.SubstringIndexBottom;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+@Service
+@Slf4j
+public class RawScoreService {
+
+    private static final int CWID_IDX = 2;
+    private static final int SCORE_IDX = 6;
+    private static final int STATUS_IDX = 8;
+    private static final int SUBMISSION_TIME_IDX = 10;
+    private static final int HOURS_LATE_IDX = 11;
+
+    private final RawScoreRepo rawScoreRepo;
+
+    public RawScoreService(RawScoreRepo rawScoreRepo){
+        this.rawScoreRepo = rawScoreRepo;
+    }
+
+    public List<RawScore> uploadCSV(MultipartFile file, UUID migrationId) {
+        List<RawScore> scores = new LinkedList<>();
+
+        if (file.isEmpty()){
+            log.warn("Provided file {} is empty", file.getName());
+            return scores;
+        }
+        if(file.getContentType() == null){
+            log.warn("File content type not defined for file {}", file.getName());
+            return scores;
+        }
+        if (!file.getContentType().equals("text/csv")){
+            log.warn("File MIME type is not test/csv for the file {}", file.getName());
+            return scores;
+        }
+
+        try (InputStream inputStream = file.getInputStream();
+             CSVReader csvReader = new CSVReaderBuilder(new InputStreamReader(inputStream))
+                .withSkipLines(1)
+                .build()) {
+
+            String[] line;
+            while((line = csvReader.readNext()) != null){
+                RawScore rawScore = parseLineToRawScore(migrationId, line);
+                scores.add(rawScore);
+            }
+        }
+        catch (Exception e){
+            log.warn(e.getMessage());
+        }
+
+        return scores;
+    }
+
+    private RawScore parseLineToRawScore(UUID migrationId, String[] line){
+
+        String cwid = line[CWID_IDX];
+
+        String status = line[STATUS_IDX].trim();
+
+        if(status.equals("Missing"))
+            return createOrUpdateRawScore(migrationId, cwid, null, null, null, SubmissionStatus.MISSING);
+
+        double score = Double.parseDouble(line[SCORE_IDX]);
+
+        Instant submissionTime = LocalDateTime.parse(
+                line[SUBMISSION_TIME_IDX],
+                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss Z", Locale.US)
+        ).atZone(
+                ZoneId.of("America/Denver")
+        ).toInstant();
+
+        double hoursLate = 0.0;
+        String[] lateTime = line[HOURS_LATE_IDX].split(":");
+        hoursLate += Double.parseDouble(lateTime[0]);
+        hoursLate += Double.parseDouble(lateTime[1])/60;
+        hoursLate += Double.parseDouble(lateTime[2])/3600;
+
+        SubmissionStatus submissionStatus = SubmissionStatus.ON_TIME;
+        if(hoursLate > 0)
+            submissionStatus = SubmissionStatus.LATE;
+
+        return createOrUpdateRawScore(migrationId, cwid, score, submissionTime, hoursLate, submissionStatus);
+
+    }
+
+    public RawScore createOrUpdateRawScore(UUID migrationId, String cwid, Double score, Instant submissionTime, Double hoursLate, SubmissionStatus submissionStatus){
+        RawScore rawScore;
+        if(rawScoreRepo.existsByCwidAndMigrationId(cwid, migrationId)){
+            Optional<RawScore> oldRawScore = rawScoreRepo.getByCwidAndMigrationId(cwid, migrationId);
+            rawScore = updateRawScore(oldRawScore.get(), score, submissionTime, hoursLate, submissionStatus);
+        }else{
+            rawScore = createRawScore(migrationId, cwid, score, submissionTime, hoursLate, submissionStatus);
+        }
+        return rawScoreRepo.save(rawScore);
+    }
+
+    private RawScore updateRawScore(RawScore rawScore, Double score, Instant submissionTime, Double hoursLate, SubmissionStatus submissionStatus){
+        UUID migrationId = rawScore.getMigrationId();
+        String cwid = rawScore.getCwid();
+
+        log.warn("Overwriting raw score for migration {} with user {}", migrationId, cwid);
+        rawScore.setSubmissionStatus(submissionStatus);
+        rawScore.setMigrationId(migrationId);
+        rawScore.setCwid(cwid);
+        rawScore.setScore(score);
+        rawScore.setSubmissionTime(submissionTime);
+        rawScore.setHoursLate(hoursLate);
+        rawScore.setSubmissionStatus(submissionStatus);
+        return rawScore;
+    }
+
+    private RawScore createRawScore(UUID migrationId, String cwid, Double score, Instant submissionTime, Double hoursLate, SubmissionStatus submissionStatus){
+        RawScore newScore = new RawScore();
+        newScore.setMigrationId(migrationId);
+        newScore.setCwid(cwid);
+        newScore.setScore(score);
+        newScore.setSubmissionTime(submissionTime);
+        newScore.setHoursLate(hoursLate);
+        newScore.setSubmissionStatus(submissionStatus);
+        return newScore;
+    }
+
+    public Optional<RawScore> getRawScoreFromCwidAndAssignmentId(String cwid, UUID migrationId){
+        Optional<RawScore> score = rawScoreRepo.getByCwidAndMigrationId(cwid, migrationId);
+        if(score.isEmpty()){
+            log.warn("Could not find raw score for cwid {} on migration id {}", cwid, migrationId);
+            return Optional.empty();
+        }
+        return score;
+    }
+
+}
---
+++ grading-admin-service/src/test/java/edu/mines/gradingadmin/services/TestRawScoreService.java
@@ -0,0 +1,281 @@
+package edu.mines.gradingadmin.services;
+
+import edu.mines.gradingadmin.containers.PostgresTestContainer;
+import edu.mines.gradingadmin.models.RawScore;
+import edu.mines.gradingadmin.models.SubmissionStatus;
+import edu.mines.gradingadmin.repositories.RawScoreRepo;
+import jakarta.transaction.Transactional;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@SpringBootTest
+@Transactional
+public class TestRawScoreService implements PostgresTestContainer {
+
+    @Autowired
+    private RawScoreRepo rawScoreRepo;
+
+    @Autowired
+    private RawScoreService rawScoreService;
+
+    @BeforeAll
+    static void setupClass(){
+        postgres.start();
+    }
+
+    @AfterEach
+    void tearDown(){
+        rawScoreRepo.deleteAll();
+    }
+
+    @Test
+    @SneakyThrows
+    void testEmpty(){
+        String fileContent = "";
+
+        String filename = "test.csv";
+        MockMultipartFile file = new MockMultipartFile(filename, filename, "text/csv", fileContent.getBytes());
+        UUID testId = UUID.randomUUID();
+
+        List<RawScore> rawScores = rawScoreService.uploadCSV(file, testId);
+
+        Assertions.assertTrue(rawScores.isEmpty());
+    }
+
+    @Test
+    @SneakyThrows
+    void testSkipFirst(){
+        String fileContent = "First Name,Last Name,SID,,,,Total Score,Max Points,Status,,Submission Time,Lateness (H:M:S)\n";
+
+        String filename = "test.csv";
+        MockMultipartFile file = new MockMultipartFile(filename, filename, "text/csv", fileContent.getBytes());
+        UUID testId = UUID.randomUUID();
+
+        List<RawScore> rawScores = rawScoreService.uploadCSV(file, testId);
+
+        Assertions.assertTrue(rawScores.isEmpty());
+    }
+
+    @Test
+    @SneakyThrows
+    void testParse(){
+
+        String fileContent = "First Name,Last Name,SID,,,,Total Score,Max Points,Status,,Submission Time,Lateness (H:M:S)\n" +
+                "Jane,Doe,12344321,,,,12.0,12.0,Graded,,2022-06-25 13:16:26 -0600,13:29:30\n" +
+                "Tester,Testing,testtest,,,,12.0,12.0,Graded,,2022-06-25 13:16:58 -0600,00:00:00\n" +
+                "Jimmy,yyy,jimmyyyy,,,,11.5,12.0,Graded,,2022-06-25 13:25:12 -0600,07:32:50\n" +
+                "Joe,Jam,121212,,,,,12.0,Missing\n";
+
+        String filename = "test.csv";
+        MockMultipartFile file = new MockMultipartFile(filename, filename, "text/csv", fileContent.getBytes());
+        UUID testId = UUID.randomUUID();
+
+        // Should add scores to the list that were properly saved
+        List<RawScore> scores = rawScoreService.uploadCSV(file, testId);
+        Assertions.assertFalse(scores.isEmpty());
+
+        // Singular test get on saved score
+        Optional<RawScore> score = rawScoreService.getRawScoreFromCwidAndAssignmentId("12344321", testId);
+        Assertions.assertFalse(score.isEmpty());
+        Assertions.assertEquals(SubmissionStatus.LATE, score.get().getSubmissionStatus());
+
+        // Test weird conversion fields
+        score = rawScoreService.getRawScoreFromCwidAndAssignmentId("121212", testId);
+        Assertions.assertNull(score.get().getScore());
+        Assertions.assertEquals(SubmissionStatus.MISSING, score.get().getSubmissionStatus());
+        Assertions.assertNull(score.get().getHoursLate());
+        Assertions.assertNull(score.get().getSubmissionTime());
+
+        // Test weird conversion fields
+        score = rawScoreService.getRawScoreFromCwidAndAssignmentId("jimmyyyy", testId);
+        Assertions.assertEquals(11.5, score.get().getScore());
+        Assertions.assertEquals(7.5472, score.get().getHoursLate(), 0.001);
+
+        LocalDateTime localDateTime = LocalDateTime.of(2022, 6, 25, 13, 25, 12);
+        ZoneOffset zoneOffset = ZoneOffset.of("-06:00");
+        Instant instant = localDateTime.toInstant(zoneOffset);
+
+        Assertions.assertEquals(instant, score.get().getSubmissionTime());
+
+    }
+
+    @Test
+    @SneakyThrows
+    void testOverwrite(){
+        String fileContent = "First Name,Last Name,SID,,,,Total Score,Max Points,Status,,Submission Time,Lateness (H:M:S)\n" +
+                "Jane,Doe,12344321,,,,0.0,12.0,Graded,,2022-06-25 13:16:26 -0600,13:29:30\n";
+
+        String filename = "test.csv";
+        MockMultipartFile file = new MockMultipartFile(filename, filename, "text/csv", fileContent.getBytes());
+        UUID testId = UUID.randomUUID();
+
+        RawScore firstRawScore = rawScoreService.uploadCSV(file, testId).getFirst();
+
+        fileContent = "First Name,Last Name,SID,,,,Total Score,Max Points,Status,,Submission Time,Lateness (H:M:S)\n" +
+                "Jane,Doe,12344321,,,,12.0,12.0,Graded,,2022-07-25 23:20:26 -0600,15:29:30\n";
+
+        file = new MockMultipartFile(filename, filename, "text/csv", fileContent.getBytes());
+        testId = UUID.randomUUID();
+
+        RawScore secondRawScore = rawScoreService.uploadCSV(file, testId).getFirst();
+
+        Assertions.assertNotEquals(firstRawScore.getScore(), secondRawScore.getScore());
+        Assertions.assertNotEquals(firstRawScore.getSubmissionTime(), secondRawScore.getSubmissionTime());
+        Assertions.assertNotEquals(firstRawScore.getHoursLate(), secondRawScore.getHoursLate(), 0.0001);
+
+    }
+
+    @Test
+    @SneakyThrows
+    void testAllGraded(){
+        String fileContent = "First Name,Last Name,SID,Email,Sections,section_name,Total Score,Max Points,Status,Submission ID,Submission Time,Lateness (H:M:S)\n" +
+                "Samual,Mcsam,101,samualmcsam@mines.edu,,,8.0,12.0,Graded,128746829,2022-06-25 13:16:26 -0600,00:00:00\n" +
+                "Robert,Bob,robbob,robbob@mines.edu,,,6.0,12.0,Graded,128746844,2022-06-25 13:16:58 -0600,00:00:00\n" +
+                "Null,IdontNull,abcdefg,nullnullnull@mymail.mines.edu,,,12.0,12.0,Graded,128746851,2022-06-25 13:17:12 -0600,02:00:00";
+
+        String filename = "test.csv";
+        MockMultipartFile file = new MockMultipartFile(filename, filename, "text/csv", fileContent.getBytes());
+        UUID testId = UUID.randomUUID();
+
+        List<RawScore> rawScoreList = rawScoreService.uploadCSV(file, testId);
+
+        Assertions.assertEquals("101", rawScoreList.get(0).getCwid());
+        Assertions.assertEquals("robbob", rawScoreList.get(1).getCwid());
+        Assertions.assertEquals("abcdefg", rawScoreList.get(2).getCwid());
+
+        LocalDateTime localDateTime = LocalDateTime.of(2022, 6, 25, 13, 16, 26);
+        ZoneOffset zoneOffset = ZoneOffset.of("-06:00");
+        Instant instant = localDateTime.toInstant(zoneOffset);
+
+        Assertions.assertEquals(instant, rawScoreList.get(0).getSubmissionTime());
+
+        localDateTime = LocalDateTime.of(2022, 6, 25, 13, 16, 58);
+        instant = localDateTime.toInstant(zoneOffset);
+
+        Assertions.assertEquals(instant, rawScoreList.get(1).getSubmissionTime());
+
+        localDateTime = LocalDateTime.of(2022, 6, 25, 13, 17, 12);
+        instant = localDateTime.toInstant(zoneOffset);
+
+        Assertions.assertEquals(instant, rawScoreList.get(2).getSubmissionTime());
+
+        Assertions.assertNotNull(rawScoreList.get(0));
+        Assertions.assertNotNull(rawScoreList.get(1));
+        Assertions.assertNotNull(rawScoreList.get(2));
+
+        Assertions.assertEquals(8.0, rawScoreList.get(0).getScore());
+        Assertions.assertEquals(6.0, rawScoreList.get(1).getScore());
+        Assertions.assertEquals(12.0, rawScoreList.get(2).getScore());
+
+        Assertions.assertEquals(SubmissionStatus.ON_TIME, rawScoreList.get(0).getSubmissionStatus());
+        Assertions.assertEquals(SubmissionStatus.ON_TIME, rawScoreList.get(1).getSubmissionStatus());
+        Assertions.assertEquals(SubmissionStatus.LATE, rawScoreList.get(2).getSubmissionStatus());
+
+        Assertions.assertEquals(0, rawScoreList.get(0).getHoursLate());
+        Assertions.assertEquals(0, rawScoreList.get(1).getHoursLate());
+        Assertions.assertEquals(2, rawScoreList.get(2).getHoursLate());
+
+    }
+
+    @Test
+    @SneakyThrows
+    void testAllUngraded(){
+        String fileContent = "First Name,Last Name,SID,Email,Sections,section_name,Total Score,Max Points,Status,Submission ID,Submission Time,Lateness (H:M:S)\n" +
+                "Samual,Mcsam,101,samualmcsam@mines.edu,,,6.0,12.0,Ungraded,128746829,2022-06-25 13:16:26 -0600,00:00:00\n" +
+                "Robert,Bob,robbob,robbob@mines.edu,,,4.0,12.0,Ungraded,128746844,2022-06-25 13:16:58 -0600,00:00:00\n" +
+                "Null,IdontNull,abcdefg,nullnullnull@mymail.mines.edu,,,0.0,12.0,Ungraded,128746851,2022-06-25 13:17:12 -0600,02:00:00";
+
+        String filename = "test.csv";
+        MockMultipartFile file = new MockMultipartFile(filename, filename, "text/csv", fileContent.getBytes());
+        UUID testId = UUID.randomUUID();
+
+        List<RawScore> rawScoreList = rawScoreService.uploadCSV(file, testId);
+
+        Assertions.assertEquals("101", rawScoreList.get(0).getCwid());
+        Assertions.assertEquals("robbob", rawScoreList.get(1).getCwid());
+        Assertions.assertEquals("abcdefg", rawScoreList.get(2).getCwid());
+
+        LocalDateTime localDateTime = LocalDateTime.of(2022, 6, 25, 13, 16, 26);
+        ZoneOffset zoneOffset = ZoneOffset.of("-06:00");
+        Instant instant = localDateTime.toInstant(zoneOffset);
+
+        Assertions.assertEquals(instant, rawScoreList.get(0).getSubmissionTime());
+
+        localDateTime = LocalDateTime.of(2022, 6, 25, 13, 16, 58);
+        instant = localDateTime.toInstant(zoneOffset);
+
+        Assertions.assertEquals(instant, rawScoreList.get(1).getSubmissionTime());
+
+        localDateTime = LocalDateTime.of(2022, 6, 25, 13, 17, 12);
+        instant = localDateTime.toInstant(zoneOffset);
+
+        Assertions.assertEquals(instant, rawScoreList.get(2).getSubmissionTime());
+
+        Assertions.assertNotNull(rawScoreList.get(0));
+        Assertions.assertNotNull(rawScoreList.get(1));
+        Assertions.assertNotNull(rawScoreList.get(2));
+
+        Assertions.assertEquals(6.0, rawScoreList.get(0).getScore());
+        Assertions.assertEquals(4.0, rawScoreList.get(1).getScore());
+        Assertions.assertEquals(0.0, rawScoreList.get(2).getScore());
+
+        Assertions.assertEquals(SubmissionStatus.ON_TIME, rawScoreList.get(0).getSubmissionStatus());
+        Assertions.assertEquals(SubmissionStatus.ON_TIME, rawScoreList.get(1).getSubmissionStatus());
+        Assertions.assertEquals(SubmissionStatus.LATE, rawScoreList.get(2).getSubmissionStatus());
+
+        Assertions.assertEquals(0, rawScoreList.get(0).getHoursLate());
+        Assertions.assertEquals(0, rawScoreList.get(1).getHoursLate());
+        Assertions.assertEquals(2, rawScoreList.get(2).getHoursLate());
+
+    }
+
+    @Test
+    @SneakyThrows
+    void testAllMissing() {
+        String fileContent = "First Name,Last Name,SID,Email,Sections,section_name,Total Score,Max Points,Status,Submission ID,Submission Time,Lateness (H:M:S)\n" +
+                "Samual,Mcsam,101,samualmcsam@mines.edu,,,,12.0,Missing,128746829,,\n" +
+                "Robert,Bob,robbob,robbob@mines.edu,,,,12.0,Missing,128746844,,\n" +
+                "Null,IdontNull,abcdefg,nullnullnull@mymail.mines.edu,,,,12.0,Missing,128746851,,";
+
+        String filename = "test.csv";
+        MockMultipartFile file = new MockMultipartFile(filename, filename, "text/csv", fileContent.getBytes());
+        UUID testId = UUID.randomUUID();
+
+        List<RawScore> rawScoreList = rawScoreService.uploadCSV(file, testId);
+
+        Assertions.assertEquals("101", rawScoreList.get(0).getCwid());
+        Assertions.assertEquals("robbob", rawScoreList.get(1).getCwid());
+        Assertions.assertEquals("abcdefg", rawScoreList.get(2).getCwid());
+
+        Assertions.assertNull(rawScoreList.get(0).getSubmissionTime());
+        Assertions.assertNull(rawScoreList.get(1).getSubmissionTime());
+        Assertions.assertNull(rawScoreList.get(2).getSubmissionTime());
+
+        Assertions.assertNull(rawScoreList.get(0).getScore());
+        Assertions.assertNull(rawScoreList.get(1).getScore());
+        Assertions.assertNull(rawScoreList.get(2).getScore());
+
+        Assertions.assertNull(rawScoreList.get(0).getHoursLate());
+        Assertions.assertNull(rawScoreList.get(1).getHoursLate());
+        Assertions.assertNull(rawScoreList.get(2).getHoursLate());
+
+        Assertions.assertEquals(SubmissionStatus.MISSING, rawScoreList.get(0).getSubmissionStatus());
+        Assertions.assertEquals(SubmissionStatus.MISSING, rawScoreList.get(1).getSubmissionStatus());
+        Assertions.assertEquals(SubmissionStatus.MISSING, rawScoreList.get(2).getSubmissionStatus());
+
+        Assertions.assertNotNull(rawScoreList.get(0));
+        Assertions.assertNotNull(rawScoreList.get(1));
+        Assertions.assertNotNull(rawScoreList.get(2));
+    }
+
+}
