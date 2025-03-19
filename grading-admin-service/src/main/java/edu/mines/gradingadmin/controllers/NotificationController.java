8bd2e02be082d674ae509f883ea180d4ad1f0413
Gregory Bell <trihard.studios@outlook.com>
2025-03-19 13:40:46 -0600

    work on task feedback

--- grading-admin-service/src/main/java/edu/mines/gradingadmin/controllers/NotificationController.java
+++ grading-admin-service/src/main/java/edu/mines/gradingadmin/controllers/NotificationController.java
@@ -1,18 +1,12 @@
 package edu.mines.gradingadmin.controllers;
 
-import edu.mines.gradingadmin.data.TaskDTO;
 import edu.mines.gradingadmin.managers.ImpersonationManager;
-import edu.mines.gradingadmin.managers.SecurityManager;
 import edu.mines.gradingadmin.models.User;
 import edu.mines.gradingadmin.services.TaskExecutorService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
@@ -32,13 +26,12 @@ public class NotificationController {
     }
 
     @SubscribeMapping("/tasks/{task_id}")
-    @SendToUser("/tasks/{task_id}")
     public String getTaskNotifications(Principal principal, @DestinationVariable("task_id") long taskId){
         User user = impersonationManager.impersonateUser(principal).getUser();
 
         log.debug("{} to {}", user.getCwid(), taskId);
 
-        return toDto(taskExecutorService.getScheduledTask(user, taskId).get()).toString();
+        return String.valueOf(taskId);
     }
 
 
