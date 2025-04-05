c0352f77ec1afb09b95623a844f1238c0d18577a
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-04-05 17:00:35 -0600

    Implement turborepo stuff

---
+++ grading-admin-frontend/apps/student/vite.config.ts
@@ -0,0 +1,34 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // https://github.com/tabler/tabler-icons/issues/1233#issuecomment-2428245119
+      // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+      "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
+  server: {
+    host: true,
+    port: 5175,
+    origin: "http://localhost:5175",
+    proxy: {
+      "/api": {
+        target: "http://localhost.dev",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
