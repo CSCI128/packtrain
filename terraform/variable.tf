6689c1b4abcc9d17cce71a8c34c507a50267e47e
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-04-17 15:48:32 -0600

    fix url

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -79,7 +79,7 @@ variable "frontend_default_environment" {
   default = [
     { name = "VITE_OAUTH_URL", value = "https://packtrain.gregory-bell.com/auth/application/o/grading-admin/" },
     { name = "VITE_REDIRECT_URI", value = "https://packtrain.gregory-bell.com/select" },
-    { name = "VITE_LOGOUT_REDIRECT_URI", value = "https://packtrain.gregory-bell.com.com/" },
+    { name = "VITE_LOGOUT_REDIRECT_URI", value = "https://packtrain.gregory-bell.com/" },
     { name = "VITE_API_URL", value = "https://packtrain.gregory-bell.com/api/" },
     { name = "VITE_AUTH_SCOPES", value = "openid is_admin cwid email profile offline_access" },
     { name = "VITE_CLIENT_ID", value = "grading_admin_provider" }
