d046b42a0e46e9199569ebd14013016357b222a2
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-08-18 11:26:02 -0600

    Add migration middleware

--- packtrain-frontend/apps/instructor/src/pages/migration/MigrationMiddleware.tsx
+++ packtrain-frontend/apps/instructor/src/pages/migration/MigrationMiddleware.tsx
@@ -12,6 +12,7 @@ export const MigrationMiddleware = ({
   children: JSX.Element;
 }) => {
   const navigate = useNavigate();
+  const currentPage = location.pathname;
 
   const {
     data: migrationData,
@@ -38,8 +39,23 @@ export const MigrationMiddleware = ({
     navigate("/instructor/migrate");
   }
 
-  if (!migrationIsLoading) {
-    console.log("MIDDLEWARE DATA:", migrationData);
+  // Disallow moving forward in migration flow if state is not ready
+  if (!migrationIsLoading && migrationData) {
+    const status = migrationData.status;
+
+    if (currentPage !== "/instructor/migrate/load" && status === "created") {
+      navigate("/instructor/migrate/load");
+    } else if (
+      currentPage !== "/instructor/migrate/apply" &&
+      status === "loaded"
+    ) {
+      navigate("/instructor/migrate/apply");
+    } else if (
+      currentPage !== "/instructor/migrate/review" &&
+      status === "started"
+    ) {
+      navigate("/instructor/migrate/review");
+    }
   }
 
   return children;
