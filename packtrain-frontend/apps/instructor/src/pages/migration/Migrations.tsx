87bb438f1c870960c78357359da42d2b20ebd56d
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-08-25 20:05:35 -0600

    Add scroll view for migrations

--- packtrain-frontend/apps/instructor/src/pages/migration/Migrations.tsx
+++ packtrain-frontend/apps/instructor/src/pages/migration/Migrations.tsx
@@ -1,4 +1,12 @@
-import { Box, Button, Container, Divider, Group, Text } from "@mantine/core";
+import {
+  Box,
+  Button,
+  Container,
+  Divider,
+  Group,
+  ScrollArea,
+  Text,
+} from "@mantine/core";
 import { getApiClient } from "@repo/api/index";
 import { MasterMigration, Migration } from "@repo/api/openapi";
 import { store$ } from "@repo/api/store";
@@ -53,33 +61,37 @@ export function MigrationsPage() {
 
         <Divider my="sm" />
 
-        {data.length > 0 ? (
-          data.map((migration: MasterMigration) => (
-            <Box pb={20} key={migration.id}>
-              <Group grow>
+        <ScrollArea h={500}>
+          {data.length > 0 ? (
+            data.map((migration: MasterMigration) => (
+              <Box pb={20} key={migration.id}>
+                <Group grow>
+                  <Text>
+                    <strong>
+                      {formattedDate(
+                        new Date(migration.date_started as string)
+                      )}
+                    </strong>
+                  </Text>
+                  <Text>
+                    <strong>Status</strong>: {migration.status ?? "unknown"}
+                  </Text>
+                </Group>
                 <Text>
-                  <strong>
-                    {formattedDate(new Date(migration.date_started as string))}
-                  </strong>
+                  <strong>By</strong>: {migration.migrator?.name}
                 </Text>
                 <Text>
-                  <strong>Status</strong>: {migration.status ?? "unknown"}
+                  <strong>Migrated Assignments</strong>:{" "}
+                  {migration.migrations
+                    ?.map((migration: Migration) => migration.assignment.name)
+                    .join(", ")}
                 </Text>
-              </Group>
-              <Text>
-                <strong>By</strong>: {migration.migrator?.name}
-              </Text>
-              <Text>
-                <strong>Migrated Assignments</strong>:{" "}
-                {migration.migrations
-                  ?.map((migration: Migration) => migration.assignment.name)
-                  .join(", ")}
-              </Text>
-            </Box>
-          ))
-        ) : (
-          <Text mb={10}>No migrations were found!</Text>
-        )}
+              </Box>
+            ))
+          ) : (
+            <Text mb={10}>No migrations were found!</Text>
+          )}
+        </ScrollArea>
 
         <Divider pt={10} my="sm" />
 
