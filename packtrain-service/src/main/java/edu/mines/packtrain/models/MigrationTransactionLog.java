384e3d765cade591ce08c3f6dbcbfbaba9eb686c
Zoey <zoey.ganzhorn@gmail.com>
2025-09-05 09:46:23 -0600

    Added extensionDays field to migration log

--- packtrain-service/src/main/java/edu/mines/packtrain/models/MigrationTransactionLog.java
+++ packtrain-service/src/main/java/edu/mines/packtrain/models/MigrationTransactionLog.java
@@ -52,4 +52,7 @@ public class MigrationTransactionLog {
 
     @Column(name = "message", length = 1000)
     private String message;
+
+    @Column(name = "number_extension_days")
+    private int extensionDays = 0;
 }
