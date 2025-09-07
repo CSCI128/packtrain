fdeed7ab7ede256deff091ca1bdca948f2e6d13c
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-09-07 12:05:28 -0600

    checkstyle

--- packtrain-service/src/main/java/edu/mines/packtrain/models/User.java
+++ packtrain-service/src/main/java/edu/mines/packtrain/models/User.java
@@ -1,7 +1,12 @@
 package edu.mines.packtrain.models;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.Set;
 import java.util.UUID;
 import lombok.Data;
