aa0d28206d1268076bcc56583e0654d4023651ab
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-09-09 11:21:33 -0600

    Move ClassButton out of SelectClass

--- packtrain-frontend/packages/ui/src/pages/Select.tsx
+++ packtrain-frontend/packages/ui/src/pages/Select.tsx
@@ -15,6 +15,24 @@ import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { useAuth } from "react-oidc-context";
 
+function ClassButton({
+  enrollment,
+  switchCourse,
+}: {
+  enrollment: Enrollment;
+  switchCourse: (id: string, name: string) => void;
+}) {
+  return (
+    <Button
+      onClick={() => switchCourse(enrollment.id as string, enrollment.name)}
+    >
+      {enrollment.name} ({enrollment.term}){" "}
+      {store$.id.get() === enrollment.id && <>(selected)</>}
+      {!enrollment.enabled && <>(inactive)</>}
+    </Button>
+  );
+}
+
 export const SelectClass = ({ close }: { close?: () => void }) => {
   const auth = useAuth();
   const [checked, setChecked] = useState(false);
@@ -55,18 +73,6 @@ export const SelectClass = ({ close }: { close?: () => void }) => {
     close?.();
   };
 
-  function ClassButton(enrollment: Enrollment) {
-    return (
-      <Button
-        onClick={() => switchCourse(enrollment.id as string, enrollment.name)}
-      >
-        {enrollment.name} ({enrollment.term}){" "}
-        {store$.id.get() == enrollment.id && <>(selected)</>}
-        {!enrollment.enabled && <>(inactive)</>}
-      </Button>
-    );
-  }
-
   if (!data || isLoading) return <Loading />;
 
   if (error) return <Text>An error occured: {error.message}</Text>;
@@ -91,7 +97,11 @@ export const SelectClass = ({ close }: { close?: () => void }) => {
                         store$.id.get() === enrollment.id
                     )
                     .map((enrollment: Enrollment) => (
-                      <ClassButton key={enrollment.id} {...enrollment} />
+                      <ClassButton
+                        key={enrollment.id}
+                        enrollment={enrollment}
+                        switchCourse={switchCourse}
+                      />
                     ))}
                   <Button color="green" onClick={handleCreateClass}>
                     Create Class
@@ -112,7 +122,11 @@ export const SelectClass = ({ close }: { close?: () => void }) => {
                   {data
                     .filter((enrollment: Enrollment) => enrollment.enabled)
                     .map((enrollment: Enrollment) => (
-                      <ClassButton key={enrollment.id} {...enrollment} />
+                      <ClassButton
+                        key={enrollment.id}
+                        enrollment={enrollment}
+                        switchCourse={switchCourse}
+                      />
                     ))}
                 </>
               )}
