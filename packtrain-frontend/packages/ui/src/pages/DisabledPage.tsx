bf1efd36907dd27bd4375b28e449b21467d4a8f8
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-09-09 10:52:25 -0600

    Update disabled page styling

--- packtrain-frontend/packages/ui/src/pages/DisabledPage.tsx
+++ packtrain-frontend/packages/ui/src/pages/DisabledPage.tsx
@@ -1,4 +1,4 @@
-import { Text } from "@mantine/core";
+import { Container, Stack, Text } from "@mantine/core";
 import { useAuth } from "react-oidc-context";
 
 export const DisabledPage = () => {
@@ -6,12 +6,17 @@ export const DisabledPage = () => {
   const email = auth.user?.profile.email || "None";
   return (
     email != "None" && (
-      <>
-        <Text>
-          User '{email}' has been disabled! Contact an administrator for
-          assistance!
-        </Text>
-      </>
+      <Container size="xs">
+        <Stack>
+          <Text size="lg" fw={500} ta="center">
+            User Disabled
+          </Text>
+          <Text size="lg" ta="center">
+            User by email '{email}' has been disabled! Contact an administrator
+            for assistance!
+          </Text>
+        </Stack>
+      </Container>
     )
   );
 };
