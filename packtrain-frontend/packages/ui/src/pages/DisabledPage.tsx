a392af9161562543f9193ab8d62ca94f234da0a4
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-09-09 15:54:29 -0600

    Disabled Page: simplify logic

--- packtrain-frontend/packages/ui/src/pages/DisabledPage.tsx
+++ packtrain-frontend/packages/ui/src/pages/DisabledPage.tsx
@@ -3,17 +3,16 @@ import { useAuth } from "react-oidc-context";
 
 export const DisabledPage = () => {
   const auth = useAuth();
-  const email = auth.user?.profile.email || "None";
   return (
-    email != "None" && (
+    auth.user?.profile.email != "None" && (
       <Container size="xs">
         <Stack>
           <Text size="lg" fw={500} ta="center">
             User Disabled
           </Text>
           <Text size="lg" ta="center">
-            User by email '{email}' has been disabled! Contact an administrator
-            for assistance!
+            User by email '{auth.user?.profile.email}' has been disabled!
+            Contact an administrator for assistance!
           </Text>
         </Stack>
       </Container>
