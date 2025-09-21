df53ce6a4c2f8b659f4d4d79fe7c3ad976b9df0a
Jett Morrandez <jett_morrandez@mines.edu>
2025-09-21 14:51:56 -0600

    extension service test

--- packtrain-service/src/test/java/edu/mines/packtrain/services/TestExtensionService.java
+++ packtrain-service/src/test/java/edu/mines/packtrain/services/TestExtensionService.java
@@ -1,11 +1,22 @@
 package edu.mines.packtrain.services;
 
 import edu.mines.packtrain.containers.PostgresTestContainer;
+import edu.mines.packtrain.models.LateRequest;
+import edu.mines.packtrain.models.enums.LateRequestStatus;
 import edu.mines.packtrain.repositories.ExtensionRepo;
+import edu.mines.packtrain.repositories.LateRequestRepo;
 import edu.mines.packtrain.seeders.CourseSeeders;
+
+import static org.mockito.ArgumentMatchers.any;
+
+import java.util.UUID;
+
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -18,11 +29,14 @@ public class TestExtensionService implements PostgresTestContainer {
     private ExtensionRepo extensionRepo;
     @Autowired
     private CourseSeeders courseSeeders;
+    @Autowired
+    private LateRequestRepo lateRequestRepo;
 
     @BeforeAll
     static void setupClass(){
         postgres.start();
     }
+
 
     @AfterEach
     void tearDown(){
@@ -35,6 +49,20 @@ public class TestExtensionService implements PostgresTestContainer {
     @Test
     void verifyGetAllExtensions(){
 
+
+    }
+
+    @Test
+    void verifyProcessExtensionApplied(){
+        Mockito.when(lateRequestRepo.getLateRequestById(any())).thenReturn(null);
+
+        extensionService.processExtensionApplied(UUID.randomUUID(), true, 2);
+        ArgumentCaptor<LateRequest> requestCaptor = ArgumentCaptor.forClass(LateRequest.class);
+        Mockito.verify(lateRequestRepo).save(requestCaptor.capture());
+
+        LateRequest capturedRequest = requestCaptor.getValue();
+        Assertions.assertEquals(capturedRequest.getStatus(), LateRequestStatus.APPLIED);
+        Assertions.assertEquals(capturedRequest.getDaysRequested(), 2);
 
     }
 
